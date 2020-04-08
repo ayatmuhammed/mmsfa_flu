c295c59f7b401628a569c462cc5be2c09d825120
break a leg <nonahuda26@gmail.com>
2020-04-09 02:15:09 +0300

    utils order

--- lib/database/controller/Auth.dart
+++ lib/database/controller/Auth.dart
@@ -19,15 +19,26 @@ class Auth implements BaseAuth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   Future<String> signIn(String email, String password) async {
-    FirebaseUser user = (await _firebaseAuth.signInWithEmailAndPassword(
-        email: email, password: password)).user;
-    return user.uid;
+    try {
+      FirebaseUser user = (await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password)).user;
+
+      return user.uid;
+    }on Exception catch(e){
+        return null;
+    }
+
   }
 
   Future<String> signUp(String email, String password) async {
-    FirebaseUser user =( await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password)).user;
-    return user.uid;
+    try {
+      FirebaseUser user =( await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password)).user;
+
+      return user.uid;
+    }on Exception catch(e){
+      return null;
+    }
   }
 
   Future<FirebaseUser> getCurrentUser() async {
