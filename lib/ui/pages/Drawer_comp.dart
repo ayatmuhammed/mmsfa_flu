2703260f4a58efa1275ce7ebd96706ef986a5721
break a leg <nonahuda26@gmail.com>
2020-04-15 18:19:23 +0300

    update the drawer

--- lib/ui/pages/Drawer_comp.dart
+++ lib/ui/pages/Drawer_comp.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -23,89 +22,87 @@ File image;
     }
   }
 
-//  File galleryFile;
-//  imageSelectorGallery() async {
-//    galleryFile = await ImagePicker.pickImage(
-//      source: ImageSource.gallery,
-//    );
-//    if(img !=null){
-//      galleryFile=File.img;
-//    }
-//
-//    print("You selected gallery image : " + galleryFile.path);
-//    setState(() {});
-//  }
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            DrawerHeader(
-              child: Column(
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundColor: Colors.indigo[100],
-                    child: FlatButton(
-                      onPressed: () {
-                        Image.file(image);
-                      },
-                      child: Icon(Icons.image),
-                    ),
-                    radius: 60.0,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left:150.0,top: 10.0),
+                  child: Text('More',style: TextStyle(color: Colors.indigo,fontSize: 18.0),
                   ),
-                  SizedBox(
-                    height: 2.0,
+                ),
+                SizedBox(height:8.0,),
+                Divider(
+                  color: Colors.grey,
+                ),
+                SizedBox(height:8.0,),
+                CircleAvatar(
+                  backgroundColor: Colors.indigo[100],
+                  child: FlatButton(
+                    onPressed: () {
+                      Image.file(image);
+                    },
+                    child: Icon(Icons.image),
                   ),
-                  Text('ayatprogram@gmail.com', style: TextStyle(
+                  radius: 60.0,
+                ),
+                SizedBox(
+                  height:10.0,
+                ),
+                Text('ayatprogram@gmail.com', style: TextStyle(
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.bold, color: Colors.indigo),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Divider(color: Colors.grey,height:10.0,),
+                FlatButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => QrGenerator(),
+                      ),
+                    );
+                  },
+                  child: Text('- QR Generate', style: TextStyle(
                       fontSize: 12.0,
                       fontWeight: FontWeight.bold, color: Colors.indigo),
                   ),
+                ),
+                SizedBox(
+                  height: 2.0,
+                ),
 
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            FlatButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => QrGenerator(),
-                  ),
-                );
-              },
-              child: Text('- QR Generate', style: TextStyle(
-                  fontSize: 12.0,
-                  fontWeight: FontWeight.bold, color: Colors.indigo),),
-            ),
-            SizedBox(
-              height: 2.0,
-            ),
+                FlatButton(
+                  onPressed: () async {
+                    SharedPreferences prefs = await SharedPreferences.getInstance();
+                    await prefs.remove('userPreference');
+                    await Future.delayed(Duration(seconds: 2));
 
-            FlatButton(
-              onPressed: () async {
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                await prefs.remove('userPreference');
-                await Future.delayed(Duration(seconds: 2));
+                    Navigator.of(context).pushAndRemoveUntil(
+                      // the new route
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => LoginSignInPage(auth: Auth(),),
+                      ),
+                          (Route route) => false,
+                    );
+                  },
+                  child: Text('-Logout', style: TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.bold, color: Colors.indigo),),
+                ),
 
-                Navigator.of(context).pushAndRemoveUntil(
-                  // the new route
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => LoginSignInPage(auth: Auth(),),
-                  ),
-                      (Route route) => false,
-                );
-              },
-              child: Text('-Logout', style: TextStyle(
-                  fontSize: 12.0,
-                  fontWeight: FontWeight.bold, color: Colors.indigo),),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
