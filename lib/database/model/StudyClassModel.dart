442c961e89cf77f21ad664a831a4dc10d5b7d75e
break a leg <nonahuda26@gmail.com>
2020-06-13 10:22:29 +0300

    6

--- lib/database/model/StudyClassModel.dart
+++ lib/database/model/StudyClassModel.dart
@@ -1,13 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:mmsfa_flu/utils/DatabaseSchema.dart';
 
 class StudyClassModel{
   String classId;
   String className;
 
+  StudyClassModel(this.classId, this.className);
+
   StudyClassModel.fromSnapshot(DocumentSnapshot snapshot):
       classId= snapshot.documentID,
-      className= snapshot.data["className"];
+      className= snapshot.data[ClassesCollection.CLASS_NAME_FIELD];
+
+  Map<String, dynamic> toJson() => {
+    ClassesCollection.CLASS_NAME_FIELD: className,
+    };
 
   @override
   String toString() {
