2a7ff869907b530501af65733d1028a71983e7b7
break a leg <nonahuda26@gmail.com>
2020-06-13 10:21:28 +0300

    1

--- lib/utils/DatabaseSchema.dart
+++ lib/utils/DatabaseSchema.dart
@@ -1,13 +1,14 @@
 
 class StudentsCollection{
-  static const COL_NAME= "students";
+  static const NAME= "students";
 }
 
 class TeachersCollection{
-  static const COL_NAME= "teachers";
+  static const NAME= "teachers";
   static const TEACHER_NAME_FIELD= "teacherName";
 }
 
 class ClassesCollection{
-  static const COL_NAME= "classes";
+  static const NAME= "classes";
+  static const CLASS_NAME_FIELD= "className";
 }
